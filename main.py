8a71c8a68e337cd91a467a348ff49d70adb60f9c
art <monkeyking1984@yandex.ru>
2022-04-17 18:20:01 +0300

    initial commit crypto loader

--- main.py
+++ main.py
@@ -6,38 +6,65 @@ import datetime as dt
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-class crypro_loader:
+class CryproLoader:
 
     currency = "USD"
     metric = "Close"
+    crypto = ['BTC', 'ETH', 'USDT', 'LTC', 'XRP', 'DASH', 'SC']
+
     def __init__(self, start=dt.datetime(2018, 1, 1), end=dt.datetime(2018, 2, 1)):
         self.start = start
         self.end = end
 
+    def load(self):
+        colnames = []
+        first = True
 
-# currency = "USD"
-# metric = "Close"
+        for ticker in self.crypto:
+            data = web.DataReader(f"{ticker}-{self.currency}", "yahoo", self.start, self.end)
+            if first:
+                combined = data[[self.metric]].copy()
+                colnames.append(ticker)
+                combined.columns = colnames
+                first = False
+            else:
+                combined = combined.join(data[self.metric])
+                colnames.append(ticker)
+                combined.columns = colnames
 
-start = dt.datetime(2018,1,1)
-#end = dt.datetime.now()
-end = dt.datetime(2018,2,1)
+        self.crypto_course = combined
+        return combined
 
-crypto = ['BTC', 'ETH', 'USDT', 'LTC', 'XRP', 'DASH', 'SC']
-colnames = []
+def main():
+    cryptos = CryproLoader()
+    print(cryptos.load())
 
-first = True
 
-for ticker in crypto:
-    data = web.DataReader(f"{ticker}-{currency}", "yahoo", start, end)
-    if first:
-        combined = data[[metric]].copy()
-        colnames.append(ticker)
-        combined.columns = colnames
-        first = False
-    else:
-        combined = combined.join(data[metric])
-        colnames.append(ticker)
-        combined.columns = colnames
+if __name__ == "__main__":
+    main()
+# # currency = "USD"
+# # metric = "Close"
+#
+# start = dt.datetime(2018,1,1)
+# #end = dt.datetime.now()
+# end = dt.datetime(2018,2,1)
+#
+# crypto = ['BTC', 'ETH', 'USDT', 'LTC', 'XRP', 'DASH', 'SC']
+# colnames = []
+#
+# first = True
+#
+# for ticker in crypto:
+#     data = web.DataReader(f"{ticker}-{currency}", "yahoo", start, end)
+#     if first:
+#         combined = data[[metric]].copy()
+#         colnames.append(ticker)
+#         combined.columns = colnames
+#         first = False
+#     else:
+#         combined = combined.join(data[metric])
+#         colnames.append(ticker)
+#         combined.columns = colnames
 
 plt.yscale('log') # first show linear
 
